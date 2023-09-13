b4f1414c7b0179bbde17c27a57bb1a764d08366e
franciscokavcic <fco.kavcic@gmail.com>
2023-09-12 21:32:58 -0300

    bulma funcionando

--- src/App.js
+++ src/App.js
@@ -9,11 +9,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button  class="button is-dark" >ASDCAFSAFS</button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener nore ferrer"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import 'bulma/css/bulma.min.css';
+
+
+
 import reportWebVitals from './reportWebVitals';
 
 
@@ -9,6 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    
   </React.StrictMode>
 );
 
